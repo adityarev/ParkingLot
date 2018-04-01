63fbe3bb26cfbf0ef89527fe238c3bc22e5ddeae
adityarev <aditya.vritalia@gmail.com>
2018-04-01 21:59:43 +0700

    Adding Interface Class to code.cpp

--- code.cpp
+++ code.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <cstring>
 #include <map>
 #include <vector>
 #include <queue>
@@ -28,6 +29,7 @@ public :
 
 class Parking
 {
+protected :
 	int N;
 	string *pos;
 	priority_queue<int, vector<int>, greater<int> > numPQ;
@@ -153,65 +155,160 @@ public :
 	}
 };
 
+class IParking : public Parking {
+public :
+	IParking() { }
+	IParking(int N) : Parking(N) { }
+	
+	void run() {
+		string command;
+		string reg_id;
+		int no_parking;
+		string colour;
+		
+		while (cin >> command) {
+			if (command == "park") {
+				cin >> reg_id >> colour;
+				add_car(reg_id, colour);
+			}
+			else if (command == "leave") {
+				cin >> no_parking;
+				remove_car(no_parking);
+			}
+			else if (command == "status") {
+				viewStatus();
+			}
+			else if (command == "registration_numbers_for_cars_with_colour") {
+				cin >> colour;
+				get_regID_by_colour(colour);
+			}
+			else if (command == "registration_number_for_slot_number") {
+				cin >> no_parking;
+				get_regID_by_noParking(no_parking);
+			}
+			else if (command == "slot_numbers_for_cars_with_colour") {
+				cin >> colour;
+				get_noParking_by_colour(colour);
+			}
+			else if (command == "slot_number_for_registration_number") {
+				cin >> reg_id;
+				get_noParking_by_regID(reg_id);
+			}
+			else if (command == "colour_for_registration_number") {
+				cin >> reg_id;
+				get_colour_by_regID(reg_id);
+			}
+			else if (command == "colour_for_slot_number") {
+				cin >> no_parking;
+				get_colour_by_no_parking(no_parking);
+			}
+			else puts("Invalid command!");
+		}
+	}
+};
+
+class IParkingFile : public Parking {
+public :
+	IParkingFile() { }
+	IParkingFile(int N) : Parking(N) { }
+	
+	void run(FILE *inp_file) {
+		char input[101];
+		char command[51];
+		char reg_id[21];
+		int no_parking;
+		char colour[21];
+		FILE *in = inp_file;
+		
+		while (fgets(input, sizeof(input), in)) {
+			sscanf(input, "%s", command);
+			
+			if (strcmp(command,"park") == 0) {
+				sscanf(input, "%s %s %s", command, reg_id, colour);
+				add_car(toString(reg_id, strlen(reg_id)), toString(colour, strlen(colour)));
+			}
+			else if (strcmp(command,"leave") == 0) {
+				sscanf(input, "%s %d", command, &no_parking);
+				remove_car(no_parking);
+			}
+			else if (strcmp(command,"status") == 0) {
+				viewStatus();
+			}
+			else if (strcmp(command,"registration_numbers_for_cars_with_colour") == 0) {
+				sscanf(input, "%s %s", command, colour);
+				get_regID_by_colour(toString(colour, strlen(colour)));
+			}
+			else if (strcmp(command,"registration_number_for_slot_number") == 0) {
+				sscanf(input, "%s %d", command, &no_parking);
+				get_regID_by_noParking(no_parking);
+			}
+			else if (strcmp(command,"slot_numbers_for_cars_with_colour") == 0) {
+				sscanf(input, "%s %s", command, colour);
+				get_noParking_by_colour(toString(colour, strlen(colour)));
+			}
+			else if (strcmp(command,"slot_number_for_registration_number") == 0) {
+				sscanf(input, "%s %s", command, reg_id);
+				get_noParking_by_regID(toString(reg_id, strlen(reg_id)));
+			}
+			else if (strcmp(command,"colour_for_registration_number") == 0) {
+				sscanf(input, "%s %s", command, reg_id);
+				get_colour_by_regID(toString(reg_id, strlen(reg_id)));
+			}
+			else if (strcmp(command,"colour_for_slot_number") == 0) {
+				sscanf(input, "%s %d", command, &no_parking);
+				get_colour_by_no_parking(no_parking);
+			}
+			else puts("Invalid command!");
+		}
+	}
+	
+	string toString(char s[], int n) {
+		string str = "";
+		for (int i=0; i<n; i++)
+			str += s[i];
+		return str;
+	}
+};
+
 int main(int argc, char **argv)
 {
-	Parking *parkingLot;
-	int N;
-	string command;
-	
-	for (int i=0; i<argc; i++) {
-		printf ("%d -> %s\n", i+1, argv[i]);
+	if (argc<2) {
+		IParking *parkingLot;
+		
+		// Waiting for create command
+		string command;
+		while (cin >> command) {
+			if (command == "create_parking_lot") {
+				int N;
+				cin >> N;
+				parkingLot = new IParking(N);
+				break;
+			}
+			else puts("No parking lot available, please create it first!");
+		}
+		
+		parkingLot->run();
 	}
-	
-	// Waiting for create command
-	while (cin >> command) {
-		if (command == "create_parking_lot") {
-			cin >> N;
-			parkingLot = new Parking(N);
-			break;
+	else  {
+		FILE *in;
+		in = fopen(argv[1], "r");
+		IParkingFile *parkingLot;
+		
+		char input[101];
+		while (fgets(input, sizeof(input), in)) {
+			char command[51];
+			sscanf(input, "%s", command);
+			if (strcmp(command,"create_parking_lot") == 0) {
+				int N;
+				sscanf(input, "%s %d", command, &N);
+				parkingLot = new IParkingFile(N);
+				break;
+			}
+			else puts("No parking lot available, please create it first!");
 		}
-		else puts("No parking lot available, please create it first!");
-	}
-	
-	int no_parking;
-	string reg_id, colour;
-	while (cin >> command) {
-		if (command == "park") {
-			cin >> reg_id >> colour;
-			parkingLot->add_car(reg_id, colour);
-		}
-		else if (command == "leave") {
-			cin >> no_parking;
-			parkingLot->remove_car(no_parking);
-		}
-		else if (command == "status") {
-			parkingLot->viewStatus();
-		}
-		else if (command == "registration_numbers_for_cars_with_colour") {
-			cin >> colour;
-			parkingLot->get_regID_by_colour(colour);
-		}
-		else if (command == "registration_number_for_slot_number") {
-			cin >> no_parking;
-			parkingLot->get_regID_by_noParking(no_parking);
-		}
-		else if (command == "slot_numbers_for_cars_with_colour") {
-			cin >> colour;
-			parkingLot->get_noParking_by_colour(colour);
-		}
-		else if (command == "slot_number_for_registration_number") {
-			cin >> reg_id;
-			parkingLot->get_noParking_by_regID(reg_id);
-		}
-		else if (command == "colour_for_registration_number") {
-			cin >> reg_id;
-			parkingLot->get_colour_by_regID(reg_id);
-		}
-		else if (command == "colour_for_slot_number") {
-			cin >> no_parking;
-			parkingLot->get_colour_by_no_parking(no_parking);
-		}
-		else puts("Invalid command!");
+		
+		parkingLot->run(in);
+		fclose(in);
 	}
 	
 	return 0;
